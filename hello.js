53b81306439be10547d77e2277776baa09bbed3d
Kedarcl00 <kedarpatil966@gmail.com>
2026-02-13 23:50:47 +0530

    commit add2 and hello

---
+++ hello.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var add=require('./add2');
+
+http.createServer(function(req, res) {
+
+    let q = url.parse(req.url, true).query;
+
+    let num1 = parseInt(q.a);
+    let num2 = parseInt(q.b);
+
+    let sum =add(num1, num2);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`Sum is ${sum}`);
+
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
